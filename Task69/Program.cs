04160c8c9ea1cd20fa00f4cb7acb7dc4fd584fc0
Dmitrij Kardashov <karddmitrij@yandex.ru>
2023-10-21 22:38:21 +0300

    Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите первое число:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int Pov(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * Pov(a, b - 1);
+}
+
+if (numberN < 0)
+    Console.WriteLine(1/(double)Pov(numberM, -numberN));
+else
+    Console.WriteLine(Pov(numberM, numberN));

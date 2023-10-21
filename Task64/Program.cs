09d3870ccbbf2c102fbc9ae19e28c45d33239587
Dmitrij Kardashov <karddmitrij@yandex.ru>
2023-10-22 01:17:48 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+Console.Write("[ ");
+PrintNaturalNumbers(number);
+Console.Write("]");
+void PrintNaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    PrintNaturalNumbers(num - 1);
+  
+}
